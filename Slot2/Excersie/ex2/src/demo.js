0ad8ac19641a9a529d8fdaef5315177ee4bed64b
nguyenkhoi190305-cell <nguyenkhoi190305@gmail.com>
2026-05-13 16:34:08 +0700

    Update

---
+++ Slot2/Excersie/ex2/src/demo.js
@@ -0,0 +1,13 @@
+let tong = (a,b) => a + b;
+//goi ham tong voi a=5 b=3, sau do in ra ket qau ra console
+console.log(tong(5, 3));
+
+let chao = () => console.log("Xin chao");
+//goi ham chao ra de in ra loi chao
+chao();
+
+
+//viet ham Chao1 nhan 1 tham so ten va in ra loi chao voi ten do
+let chao1 = (ten) => console.log(`Xin chao, ${ten}!`);
+chao1(Khoi);
+
