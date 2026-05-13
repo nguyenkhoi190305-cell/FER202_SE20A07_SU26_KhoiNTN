fcd46523770c21a09fbc1171bc58a6c2d8224074
nguyenkhoi190305-cell <nguyenkhoi190305@gmail.com>
2026-05-13 16:13:10 +0700

    Slot2\Excersie\ex1

---
+++ Slot2/Excersie/ex1/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const name = "KhoiNTN"
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Khoi</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
